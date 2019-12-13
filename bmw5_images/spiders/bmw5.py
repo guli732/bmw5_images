b61167a342f6cf193513860750fc42a964f25123
guli732 <2087816661@qq.com>
2019-12-13 08:05:48 +0800

    所有高清图片分类保存

--- bmw5_images/spiders/bmw5.py
+++ bmw5_images/spiders/bmw5.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from bmw5_images.items import Bmw5ImagesItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class Bmw5Spider(scrapy.Spider):
+class Bmw5Spider(CrawlSpider):
     name = 'bmw5'
     allowed_domains = ['car.autohome.com.cn']
     start_urls = ['https://car.autohome.com.cn/pic/series/202.html']
 
-    def parse(self, response):
-        uiboxs = response.xpath("//div[@class='uibox']")[1:]
-        for uibox in uiboxs:
-            category = uibox.xpath(".//div[@class='uibox-title']/a[1]/text()").get()
-            urls = uibox.xpath(".//ul/li/a/img/@src").getall()
-            urls = list(map(lambda url:response.urljoin(url), urls))
-            item = Bmw5ImagesItem(category=category, image_urls=urls)
-            yield item
+    rules = (
+        Rule(LinkExtractor(allow=r'https://car.autohome.com.cn/pic/series/202.+'), callback='parse_page', follow=True),
+    )
+
+    def parse_page(self, response):
+        category = response.xpath("//div[@class='uibox']/div[@class='uibox-title']/text()").get()
+        urls = response.xpath("//div[@class='uibox-con carpic-list03 border-b-solid']//li/a/img/@src").getall()
+        urls = list(map(lambda url: response.urljoin(url.replace('240x180', '1024x0')), urls))
+        item = Bmw5ImagesItem(category=category, image_urls=urls)
+        yield item
